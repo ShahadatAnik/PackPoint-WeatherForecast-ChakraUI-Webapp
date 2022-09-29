c1f88c28d8ffb56de24e956ae721a3e6a01e8c63
Shahadat Anik <git.shahadat.anik@gmail.com>
2022-09-30 00:12:01 +0600

    design almost done

--- src/components/weather.js
+++ src/components/weather.js
@@ -11,7 +11,13 @@ import {
   VStack,
   StackDivider,
   Container,
+  useBoolean,
+  Switch,
+  Text,
+  Tooltip,
+  IconButton,
 } from '@chakra-ui/react';
+import { WiCelsius, WiFahrenheit } from 'react-icons/wi';
 import { SearchIcon } from '@chakra-ui/icons';
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
 import Select from 'react-select';
@@ -22,30 +28,70 @@ export default function Weather() {
   const options = city.map(item => {
     return { value: item, label: item };
   });
+  const [isCelcius, setIsCelcius] = useState(true);
+  console.log(isCelcius);
+  const formateOneDecimal = num => {
+    var n;
+    if (isCelcius) {
+      n = num - 273.15;
+      return n.toFixed(1);
+    } else {
+      n = ((num - 273.15) * 9) / 5 + 32;
+      return n.toFixed(1);
+    }
+  };
+
+  useEffect(() => {
+    formateOneDecimal();
+  }, [isCelcius]);
 
   return (
     <Container as="section">
-      <Flex alignItems="center" gap="2">
-        <Box p="2">
-          <Heading size="md">What to Take?</Heading>
-        </Box>
+      <Flex alignItems="center">
+        <Heading
+          as="h1"
+          size="lg"
+          noOfLines={1}
+          color="green.600"
+          fontWeight="bold"
+        >
+          CheckBeforePack
+        </Heading>
+        <Spacer />
+        <IconButton
+          size="sm"
+          as="b"
+          variant="ghost"
+          fontSize="5xl"
+          color="green.600"
+          onClick={() => setIsCelcius(!isCelcius)}
+          icon={isCelcius ? <WiFahrenheit /> : <WiCelsius />}
+        />
+
         <Spacer />
         <ColorModeSwitcher justifySelf="flex-end" />
       </Flex>
       <Stack
         Stack
         direction={['column', 'column', 'row']}
-        spacing={['6', '6', '5']}
-        divider={<StackDivider borderColor="gray.200" />}
+        spacing={['6', '6', '3']}
         justify="center"
         mt="5"
       >
         {options && (
-          <WeatherInfo props={{ city: options, placeholder: 'From' }} />
+          <WeatherInfo
+            city={options}
+            placeholder={'From'}
+            doCelcius={formateOneDecimal}
+          />
         )}
 
         {options && (
-          <WeatherInfo props={{ city: options, placeholder: 'To' }} />
+          <WeatherInfo
+            city={options}
+            placeholder={'To'}
+            doCelcius={formateOneDecimal}
+          />
         )}
       </Stack>
     </Container>
--- src/components/weatherInfo.js
+++ src/components/weatherInfo.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import Select from 'react-select';
 import {
-  SelectContainer,
+  Flex,
   SelectList,
   SelectOption,
   SelectInputValue,
@@ -14,8 +14,17 @@ import {
   VStack,
   Stack,
   CircularProgress,
+  useBoolean,
+  Img,
+  Image,
+  Text,
+  Highlight,
+  Icon,
 } from '@chakra-ui/react';
-
+import { WiHumidity } from 'react-icons/wi';
+import { GiMightyForce, GiSunrise, GiSunset } from 'react-icons/gi';
+import { MdNaturePeople } from 'react-icons/md';
+import { FaArrowUp, FaArrowDown, FaWind } from 'react-icons/fa';
 const API = {
   key: '834bb64c903346b8196dbbd32d6ce233',
   base: 'https://api.openweathermap.org/data/2.5/',
@@ -41,33 +50,67 @@ const customStyles = {
 
   control: _ => ({
     minWidth: '250px',
+    width: '100%',
     display: 'flex',
     border: '1px solid #e2e8f0',
     borderRadius: '0.25rem',
     padding: '0.5rem 1rem',
-    color: 'green.700',
+    color: 'green.600',
     backgroundColor: 'white',
     fontSize: '1rem',
     lineHeight: '1.5',
     fontWeight: '400',
     boxShadow: '0 10px 15px 0 rgba(0, 0, 0, 0.1)',
     '&:hover': {
-      borderColor: 'green.300',
+      borderColor: 'green.600',
       boxShadow: '0 1px 3px 0 rgba(0, 0, 0, 0.1)',
     },
     '&:focus': {
-      borderColor: 'green.300',
+      borderColor: 'green.600',
       boxShadow: '0 1px 3px 0 rgba(0, 0, 0, 0.1)',
     },
   }),
 };
 
-function formateOneDecimal(num) {
-  var n = num - 273.15;
-  return n.toFixed(1);
+function Tem({ icon, children, ...rest }) {
+  return (
+    <HStack {...rest} spacing={2}>
+      <Image as={icon} boxSize={['5', '5', '7']} />
+      <Text textAlign="left" fontSize={['sm', 'md', 'lg']} as="b">
+        <Highlight
+          query={children}
+          styles={{
+            px: '4',
+            py: '2',
+            rounded: 'full',
+            bg: 'green.100',
+          }}
+        >
+          {children}
+        </Highlight>
+      </Text>
+    </HStack>
+  );
 }
 
-function WeatherInfo({ props }) {
+function UnixToTime(unix) {
+  var date = new Date(unix * 1000);
+  var hours = date.getHours();
+  var minutes = '0' + date.getMinutes();
+  var formattedTime = hours + ':' + minutes.substr(-2);
+  return formattedTime;
+}
+
+function UnixToDate(unix) {
+  var date = new Date(unix * 1000);
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
+  var formattedDate = day + '/' + month + '/' + year;
+  return formattedDate;
+}
+
+function WeatherInfo({ city, placeholder, doCelcius }) {
   const [query, setQuery] = useState('');
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -94,37 +137,86 @@ function WeatherInfo({ props }) {
   }, [query]);
 
   return (
-    <VStack>
-      <Select
-        sx={{ border: '1px solid red' }}
-        styles={customStyles}
-        options={props.city}
-        placeholder={props.placeholder}
-        closeMenuOnSelect={true}
-        onChange={e => setQuery(e.value)}
-      />
-      <Center m={3}>
-        {isLoading && <CircularProgress isIndeterminate color="green.300" />}
-        {weather.name && !isLoading && !error && (
-          <div>
-            City: {weather.name}
-            <br />
-            Country: {weather.sys.country}
-            <br />
-            Temp: {formateOneDecimal(weather.main.temp)}
-            <br />
-            feels_like: {formateOneDecimal(weather.main.feels_like)}
-            <br />
-            temp_min: {formateOneDecimal(weather.main.temp_min)}
-            <br />
-            temp_max: {formateOneDecimal(weather.main.temp_max)}
-            <br />
-            Weather: {weather.weather[0].main}
-            <br />
-          </div>
+    <Stack direction={['column', 'column', 'column']} spacing={[2, 4, 6]}>
+      <Box p={2}>
+        <Select
+          styles={customStyles}
+          options={city}
+          placeholder={placeholder}
+          closeMenuOnSelect={true}
+          onChange={e => setQuery(e.value)}
+        />
+      </Box>
+      <Box p={2}>
+        {isLoading && (
+          <Center>
+            <CircularProgress isIndeterminate color="green.600" />
+          </Center>
         )}
-      </Center>
-    </VStack>
+        {weather.name && !isLoading && !error && (
+          <Box p={5} rounded="md" boxShadow="2xl" alignContent="center">
+            <VStack spacing={['2', '2', '4']}>
+              <Box>
+                <span>
+                  <Text
+                    as="b"
+                    fontSize={['5xl', '5xl', '7xl']}
+                    color="green.600"
+                    textAlign="center"
+                  >
+                    {doCelcius(weather.main.temp)}°
+                  </Text>
+                </span>
+              </Box>
+              <HStack spacing={3}>
+                <Box>
+                  <Image
+                    src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                  />
+                </Box>
+                <Box>
+                  <Text fontSize={['2xl', '1xl', '3xl']} as="b">
+                    <Highlight
+                      query={weather.weather[0].main}
+                      styles={{
+                        px: '4',
+                        py: '2',
+                        rounded: 'full',
+                        bg: 'green.100',
+                      }}
+                    >
+                      {weather.weather[0].main}
+                    </Highlight>
+                  </Text>
+                </Box>
+              </HStack>
+
+              <HStack spacing={[2, 4, 6]} align="center">
+                <Tem icon={FaArrowUp}>
+                  {doCelcius(weather.main.temp_min) + '°'}
+                </Tem>
+                <Tem icon={MdNaturePeople}>
+                  {doCelcius(weather.main.feels_like) + '°'}
+                </Tem>
+                <Tem icon={FaArrowDown}>
+                  {doCelcius(weather.main.temp_max) + '°'}
+                </Tem>
+              </HStack>
+              <HStack spacing={[2, 4, 6]} align="center" pt={6}>
+                <Tem icon={FaWind}>{weather.wind.speed + 'mph'}</Tem>
+                <Tem icon={WiHumidity}>{weather.main.humidity + '%'}</Tem>
+                <Tem icon={GiMightyForce}>{weather.main.pressure + 'hPa'}</Tem>
+              </HStack>
+              <HStack spacing={6} align="center" pt={6}>
+                <Tem icon={GiSunrise}>{UnixToTime(weather.sys.sunrise)}</Tem>
+                {/* <Tem icon={GiSunrise}>{UnixToDate(weather.dt)}</Tem> */}
+                <Tem icon={GiSunset}>{UnixToTime(weather.sys.sunset)}</Tem>
+              </HStack>
+            </VStack>
+          </Box>
+        )}
+      </Box>
+    </Stack>
   );
 }
 
